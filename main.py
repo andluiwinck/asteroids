c319c5ba4095e1537ec116762c897dc124868db6
andluiwinck <andluiwinck@hotmail.com>
2025-11-13 13:21:19 -0300

    added constants.py and logger.py, modified .gitignore and main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids with pygame version: VERSION =", pygame.version.ver)
+    print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
